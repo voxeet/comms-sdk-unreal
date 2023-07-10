0750f3b6fb9788b34e3e744e6642cf77ed9d4a2b
Jakub Audykowicz <jakub.audykowicz@dolby.com>
2023-07-10 04:10:04 +0200

    Introduce DolbyIOVideoTexture

--- DolbyIO/Source/Private/DolbyIOVideoSink.cpp
+++ DolbyIO/Source/Private/DolbyIOVideoSink.cpp
@@ -3,15 +3,13 @@
 #include "DolbyIOVideoSink.h"
 
 #include "DolbyIOLogging.h"
+#include "DolbyIOVideoTexture.h"
 
 #include <dolbyio/comms/media_engine/video_utils.h>
 
 #include "Async/Async.h"
 #include "Engine/Texture2D.h"
 #include "Materials/MaterialInstanceDynamic.h"
-#include "RenderingThread.h"
-#include "Runtime/Launch/Resources/Version.h"
-#include "TextureResource.h"
 
 namespace DolbyIO
 {
@@ -20,89 +18,30 @@ namespace DolbyIO
 	namespace
 	{
 		constexpr auto TexParamName = "DolbyIO Frame";
-		constexpr int Stride = 4;
-
-		class FLockedTexture
-		{
-		public:
-			FLockedTexture(UTexture2D& Tex)
-			    : Tex(Tex), PlatformData(*Tex.
-#if ENGINE_MAJOR_VERSION == 4
-			                              PlatformData
-#else
-			                              GetPlatformData()
-#endif
-			                             ),
-			      Mip(PlatformData.Mips[0]), Buffer(Mip.BulkData.Lock(LOCK_READ_WRITE))
-			{
-			}
-
-			~FLockedTexture()
-			{
-				Mip.BulkData.Unlock();
-				Tex.UpdateResource();
-				FlushRenderingCommands();
-			}
-
-			void Resize(int Width, int Height)
-			{
-				Mip.SizeX = PlatformData.SizeX = Width;
-				Mip.SizeY = PlatformData.SizeY = Height;
-				Buffer = Mip.BulkData.Realloc(Width * Height * Stride);
-			}
-
-			operator uint8*()
-			{
-				return static_cast<uint8*>(Buffer);
-			}
-
-		private:
-			UTexture2D& Tex;
-			FTexturePlatformData& PlatformData;
-			FTexture2DMipMap& Mip;
-			void* Buffer;
-		};
-
-		UTexture2D* CreateEmptyTexture()
-		{
-			UTexture2D* Ret = UTexture2D::CreateTransient(1, 1, EPixelFormat::PF_B8G8R8A8);
-			Ret->AddToRoot();
-			AsyncTask(ENamedThreads::GameThread,
-			          [Ret]
-			          {
-				          FLockedTexture Tex{*Ret};
-				          FMemory::Memzero(Tex, Stride);
-			          });
-			return Ret;
-		}
 
 		void UnbindMaterialImpl(UMaterialInstanceDynamic& Material)
 		{
-			static UTexture2D* EmptyTexture = CreateEmptyTexture();
-			Material.SetTextureParameterValue(TexParamName, EmptyTexture);
+			Material.SetTextureParameterValue(TexParamName, FVideoTexture::GetEmptyTexture());
 		}
 	}
 
-	FVideoSink::FVideoSink(const FString& VideoTrackID) : Texture(CreateEmptyTexture()), VideoTrackID(VideoTrackID) {}
-
-	FVideoSink::~FVideoSink()
-	{
-		Texture->RemoveFromRoot();
-	}
+	FVideoSink::FVideoSink(const FString& VideoTrackID) : VideoTrackID(VideoTrackID) {}
 
 	UTexture2D* FVideoSink::GetTexture()
 	{
-		return Texture;
+		return Texture ? Texture->GetTexture() : nullptr;
 	}
 
 	void FVideoSink::BindMaterial(UMaterialInstanceDynamic* Material)
 	{
 		if (IsValid(Material))
 		{
-			DLB_UE_LOG("Binding material %u to video track ID %s texture %u", Material->GetUniqueID(), *VideoTrackID,
-			           Texture->GetUniqueID());
+			DLB_UE_LOG("Binding material %u to video track ID %s", Material->GetUniqueID(), *VideoTrackID);
 			Materials.Add(Material);
-			Material->SetTextureParameterValue(TexParamName, Texture);
+			if (Texture)
+			{
+				Material->SetTextureParameterValue(TexParamName, GetTexture());
+			}
 		}
 	}
 
@@ -110,8 +49,7 @@ namespace DolbyIO
 	{
 		if (Materials.Remove(Material) && IsValid(Material))
 		{
-			DLB_UE_LOG("Unbinding material %u from video track ID %s texture %u", Material->GetUniqueID(),
-			           *VideoTrackID, Texture->GetUniqueID());
+			DLB_UE_LOG("Unbinding material %u from video track ID %s", Material->GetUniqueID(), *VideoTrackID);
 			UnbindMaterialImpl(*Material);
 		}
 	}
@@ -133,44 +71,46 @@ namespace DolbyIO
 
 	void FVideoSink::handle_frame(const video_frame& VideoFrame)
 	{
+		!Texture ? CreateTexture(VideoFrame.width(), VideoFrame.height())
+		         : ResizeTexture(VideoFrame.width(), VideoFrame.height());
 		Convert(VideoFrame);
-
-		AsyncTask(ENamedThreads::GameThread,
-		          [WeakThis = weak_from_this(), Frame = VideoFrame]
-		          {
-			          if (std::shared_ptr<FVideoSink> SharedThis = WeakThis.lock())
-			          {
-				          SharedThis->Render(Frame);
-			          }
-		          });
+		AsyncTask(ENamedThreads::GameThread, [Tex = this->Texture] { Tex->Render(); });
 	}
 
-	void FVideoSink::Render(const video_frame& VideoFrame)
+	void FVideoSink::CreateTexture(int Width, int Height)
 	{
-		const int Width = VideoFrame.width();
-		const int Height = VideoFrame.height();
+		FEvent* TexCreated = FGenericPlatformProcess::GetSynchEventFromPool();
+		AsyncTask(ENamedThreads::GameThread,
+		          [=]
+		          {
+			          Texture = MakeShared<FVideoTexture>(Width, Height);
+			          TexCreated->Trigger();
 
-		if (Texture->GetSizeX() != Width || Texture->GetSizeY() != Height)
+			          for (UMaterialInstanceDynamic* Material : Materials)
+			          {
+				          if (IsValid(Material))
+				          {
+					          Material->SetTextureParameterValue(TexParamName, GetTexture());
+				          }
+			          }
+		          });
+		TexCreated->Wait();
+		FGenericPlatformProcess::ReturnSynchEventToPool(TexCreated);
+		DLB_UE_LOG("Created texture %u for video track ID %s %dx%d", GetTexture()->GetUniqueID(), *VideoTrackID, Width,
+		           Height);
+	}
+
+	void FVideoSink::ResizeTexture(int Width, int Height)
+	{
+		if (Texture->Resize(Width, Height))
 		{
-			DLB_UE_LOG("Resizing texture %u for video track ID %s: old %dx%d new %dx%d", Texture->GetUniqueID(),
-			           *VideoTrackID, Texture->GetSizeX(), Texture->GetSizeY(), Width, Height);
-			FLockedTexture Tex{*Texture};
-			Tex.Resize(Width, Height);
+			AsyncTask(ENamedThreads::GameThread,
+			          [=, Tex = this->Texture]
+			          {
+				          DLB_UE_LOG("Resizing texture %u: old %dx%d new %dx%d", Tex->GetTexture()->GetUniqueID(),
+				                     Tex->GetTexture()->GetSizeX(), Tex->GetTexture()->GetSizeY(), Width, Height);
+			          });
 		}
-
-		ENQUEUE_RENDER_COMMAND(DolbyIOUpdateTexture)
-		(
-		    [WeakThis = weak_from_this(), Width = static_cast<uint32>(Width),
-		     Height = static_cast<uint32>(Height)](FRHICommandListImmediate& RHICmdList)
-		    {
-			    if (std::shared_ptr<FVideoSink> SharedThis = WeakThis.lock())
-			    {
-				    FScopeLock Lock{&SharedThis->BufferLock};
-				    RHIUpdateTexture2D(SharedThis->Texture->GetResource()->GetTexture2DRHI(), 0,
-				                       FUpdateTextureRegion2D{0, 0, 0, 0, Width, Height}, Width * Stride,
-				                       SharedThis->Buffer.GetData());
-			    }
-		    });
 	}
 
 	void FVideoSink::Convert(const video_frame& VideoFrame)
@@ -184,9 +124,9 @@ namespace DolbyIO
 
 		const int Width = VideoFrame.width();
 		const int Height = VideoFrame.height();
+		const int DestStride = Width * FVideoTexture::Stride;
 
-		FScopeLock Lock{&BufferLock};
-		Buffer.Reserve(Width * Height * Stride);
+		FScopeLock Lock{Texture->GetBufferLock()};
 
 		enum video_frame_buffer::type VideoFrameBufferType = VideoFrameBuffer->type();
 
@@ -194,8 +134,8 @@ namespace DolbyIO
 		{
 			if (const video_frame_buffer_argb_interface* FrameARGB = VideoFrameBuffer->get_argb())
 			{
-				video_utils::format_converter::argb_copy(FrameARGB->data(), FrameARGB->stride(), Buffer.GetData(),
-				                                         Width * Stride, Width, Height);
+				video_utils::format_converter::argb_copy(FrameARGB->data(), FrameARGB->stride(), Texture->GetBuffer(),
+				                                         DestStride, Width, Height);
 			}
 		}
 		else if (VideoFrameBufferType == video_frame_buffer::type::i420)
@@ -204,7 +144,7 @@ namespace DolbyIO
 			{
 				video_utils::format_converter::i420_to_argb(
 				    FrameI420->data_y(), FrameI420->stride_y(), FrameI420->data_u(), FrameI420->stride_u(),
-				    FrameI420->data_v(), FrameI420->stride_v(), Buffer.GetData(), Width * Stride, Width, Height);
+				    FrameI420->data_v(), FrameI420->stride_v(), Texture->GetBuffer(), DestStride, Width, Height);
 			}
 		}
 		else if (VideoFrameBufferType == video_frame_buffer::type::nv12)
@@ -213,7 +153,7 @@ namespace DolbyIO
 			{
 				video_utils::format_converter::nv12_to_argb(FrameNV12->data_y(), FrameNV12->stride_y(),
 				                                            FrameNV12->data_uv(), FrameNV12->stride_uv(),
-				                                            Buffer.GetData(), Width * Stride, Width, Height);
+				                                            Texture->GetBuffer(), DestStride, Width, Height);
 			}
 		}
 #if PLATFORM_MAC
@@ -247,7 +187,7 @@ namespace DolbyIO
 				    static_cast<uint8*>(CVPixelBufferGetBaseAddressOfPlane(PixelBuffer, 0)),
 				    CVPixelBufferGetBytesPerRowOfPlane(PixelBuffer, 0),
 				    static_cast<uint8*>(CVPixelBufferGetBaseAddressOfPlane(PixelBuffer, 1)),
-				    CVPixelBufferGetBytesPerRowOfPlane(PixelBuffer, 1), Buffer.GetData(), Width * Stride, Width,
+				    CVPixelBufferGetBytesPerRowOfPlane(PixelBuffer, 1), Texture->GetBuffer(), DestStride, Width,
 				    Height);
 			}
 		}
