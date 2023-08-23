0e6dc566f4ab67c28c2173d1cbe3efd6f7349f4c
Jakub Audykowicz <jakub.audykowicz@dolby.com>
2023-08-23 15:16:11 +0200

    Refactor workflows

--- .github/actions/download_cppsdk/powershell/download_cppsdk.ps1
+++ .github/actions/download_cppsdk/powershell/download_cppsdk.ps1
@@ -1,4 +1,4 @@
 Write-Output "Download asset"
 curl -Uri ${Env:INPUT_URL}/${Env:INPUT_TAG}/${Env:INPUT_ASSET_NAME} -UseBasicParsing -Out ${Env:GITHUB_WORKSPACE}/${Env:INPUT_ASSET_NAME}
-Expand-Archive ${Env:GITHUB_WORKSPACE}/${Env:INPUT_ASSET_NAME} -DestinationPath ${Env:GITHUB_WORKSPACE}/${Env:CPP_SDK_DESTINATION}
+Expand-Archive ${Env:GITHUB_WORKSPACE}/${Env:INPUT_ASSET_NAME} -DestinationPath ${Env:GITHUB_WORKSPACE}/${Env:PLUGIN_SOURCE_DIR}
 rm ${Env:GITHUB_WORKSPACE}/${Env:INPUT_ASSET_NAME}
